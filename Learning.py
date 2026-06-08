732c702b133ba1383ed8e820146770b3f0c1a99e
OmarRazahy <omarelrazahy22@gmail.com>
2026-06-08 19:31:47 +0300

    Day 1: first python script

---
+++ Learning.py
@@ -0,0 +1,11 @@
+name="omar"
+print(f"Hello, i'm {name} and i'm learning python.")
+print("This is a simple program to demonstrate string formatting in python.")
+print("I can also do some basic math operations like addition, subtraction, multiplication and division.")
+a = 10
+b = 5
+print(f"The sum of {a} and {b} is {a + b}.")
+print(f"The difference of {a} and {b} is {a - b}.")
+print(f"The product of {a} and {b} is {a * b}.")
+print(f"The quotient of {a} and {b} is {int(a / b)}.")
+print("my goal is to earn 9k-20k per month as a freelancer in the next 3 months.")
